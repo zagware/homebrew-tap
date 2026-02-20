62db4a8ceaed4789cc171a50f24e0c385659a268
Davy McAleer <davymca@Davys-MacBook-Pro.local>
2026-02-20 15:44:06 +0000

    Initial formula for eng-team-reporter

---
+++ Formula/eng-team-reporter.rb
@@ -0,0 +1,22 @@
+class EngTeamReporter < Formula
+  desc "Generate engineering team activity reports from JIRA, GitHub, and Confluence"
+  homepage "https://github.com/zagware/eng-team-reporter"
+  version "1.0.6"
+  license :cannot_represent
+
+  if Hardware::CPU.arm?
+    url "https://github.com/zagware/eng-team-reporter/releases/download/v#{version}/eng-team-reporter-#{version}-macos-arm64.tar.gz"
+    sha256 "PLACEHOLDER_ARM64_SHA256"
+  else
+    url "https://github.com/zagware/eng-team-reporter/releases/download/v#{version}/eng-team-reporter-#{version}-macos-x64.tar.gz"
+    sha256 "PLACEHOLDER_X64_SHA256"
+  end
+
+  def install
+    bin.install "eng-team-reporter"
+  end
+
+  test do
+    assert_match "Engineering Team Reporter", shell_output("#{bin}/eng-team-reporter --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,16 @@
+# Zagware Homebrew Tap
+
+Homebrew formulae for [Zagware](https://github.com/zagware) tools.
+
+## Install
+
+```bash
+brew tap zagware/tap
+brew install eng-team-reporter
+```
+
+## Available Formulae
+
+| Formula | Description |
+|---------|-------------|
+| `eng-team-reporter` | Generate engineering team activity reports from JIRA, GitHub, and Confluence |
